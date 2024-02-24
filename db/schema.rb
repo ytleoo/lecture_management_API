d1daaa51fde2bcd4e03cf431a72c71fd212b9b6b
Saya Kubota <3yata.k.ar@gmail.com>
2024-02-24 11:43:15 +0900

    :recycle: DB制約

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,8 @@
 
 ActiveRecord::Schema[7.0].define(version: 2024_02_24_013500) do
   create_table "lectures", charset: "utf8mb4", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
+    t.string "name", null: false
+    t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
