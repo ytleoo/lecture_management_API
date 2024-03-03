9f02e3d9e5536c9a9847b9c71d75d152a14cabac
Saya Kubota <3yata.k.ar@gmail.com>
2024-03-03 16:40:16 +0900

    本番dbのreset

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,5 @@
 set -o errexit
 
 bundle install
-bundle exec rake db:drop
-bundle exec rake db:create
-bundle exec rake db:migrate
+bundle exec rake db:migrate:reset RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rake db:seed
